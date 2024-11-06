9b6b1e4e18bbfa99af313b88deebd65c64a6c000
Chandra Tritaqwa Ramadhan <lolpotch@gmail.com>
2024-11-06 17:06:26 +0700

    Player Attack collider

--- My project (1)/Assets/Scripts/Attack.cs
+++ My project (1)/Assets/Scripts/Attack.cs
@@ -1,10 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    public float lifetime = 3.0f; // Lifetime of the bullet in seconds
+    private PlayerMovement playerMovement;
+    public float delay = .2f;  // Delay in seconds
+    private bool isProcessing = true;  // Flag to prevent multiple coroutine calls
 
     void Start()
     {
+        playerMovement = GetComponentInParent<PlayerMovement>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        // Check if the other object has the "Enemy" tag
+        if (other.CompareTag("Enemy") && isProcessing && playerMovement.GetIsBoosting())
+        {
+            Debug.Log("Hit Enemy");
+            StartCoroutine(DelayedAction());
+        }
+    }
+
+    private IEnumerator DelayedAction()
+    {
+        isProcessing = false;  // Set flag to prevent multiple coroutine calls
+        yield return new WaitForSeconds(delay);  // Wait for the specified delay
+        isProcessing = true;  // Reset flag
     }
 }
--- My project (1)/Assets/Scripts/PlayerCollision.cs
+++ My project (1)/Assets/Scripts/PlayerCollision.cs
@@ -1,32 +1,5 @@
-using System.Collections;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    private PlayerMovement playerMovement;
-    public float delay = .2f;  // Delay in seconds
-    private bool isProcessing = true;  // Flag to prevent multiple coroutine calls
-
-    void Start()
-    {
-        playerMovement = transform.GetComponent<PlayerMovement>();
-    }
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        // Check if the other object has the "Enemy" tag
-        if (other.CompareTag("Enemy") && isProcessing && playerMovement.GetIsBoosting())
-        {
-            Debug.Log("Hit Enemy");
-            StartCoroutine(DelayedAction());
-        }
-    }
-
-    private IEnumerator DelayedAction()
-    {
-        isProcessing = false;  // Set flag to prevent multiple coroutine calls
-        yield return new WaitForSeconds(delay);  // Wait for the specified delay
-        isProcessing = true;  // Reset flag
-    }
-
 }
